5523879bf660b6175d7b03649670c869788bf7d8
Allen Chrisso Dhas <allencdvja@gmail.com>
2024-04-07 19:14:36 +0530

    --

--- swipeApp/views.py
+++ swipeApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 from .models import Song
 
-def song_list(request):
-    songs = Song.objects.all()
-    return render(request, 'player/song_list.html', {'songs': songs})
+def songs_list(request):
+    #songs = Song.objects.all()
+    # return render(request, 'player/song_list.html', {'songs': songs})
+    return render(request, 'songs_list.html')
